ac828770292e645babe3a3f4d8567b3384058333
hwangsungyo <gdkds12@gmail.com>
2025-05-06 01:58:31 +0900

    Update next.config.mjs

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  // reactStrictMode: true, // 필요에 따라 주석 해제 또는 설정 추가
+  output: 'standalone', // <<< standalone 출력 모드 활성화
+
   webpack: (config, { isServer }) => {
-    // 서버 측에서 Webpack이 'canvas' 모듈을 번들링하려고 시도하지 않도록 설정
+    // 서버 측 빌드에서 canvas 모듈을 외부 모듈로 처리
+    // react-pdf 사용 시 필요할 수 있음 (이전 오류 해결 과정에서 추가됨)
     if (isServer) {
       config.externals.push('canvas');
     }
